ad79a3fe3da483218418fd4e83d557c4fd68e79c
Antonio Noca <antonionoca@gmail.com>
2018-02-03 11:11:34 +0000

    Added extra homepage checks

--- homepage.spec.js
+++ homepage.spec.js
@@ -2,12 +2,24 @@
 
 describe('reddit homepage', function() {
 
-  it('should load the page and list 25 ranks', function() {
-
+  beforeAll(function() {
     browser.waitForAngularEnabled(false);
     browser.get('https://reddit.com');
+  });
 
+  it('should have 25 ranks', function() {
     expect(element.all(by.css('.rank')).count()).not.toBeLessThan(25);
   });
 
+  it('should have 25 upvote arrows', function() {
+    expect(element.all(by.css('.arrow.up')).count()).not.toBeLessThan(25);
+  });
+
+  it('should have 25 downvote arrows', function() {
+    expect(element.all(by.css('.arrow.down')).count()).not.toBeLessThan(25);
+  });
+
+  it('should have 25 titles', function() {
+    expect(element.all(by.css('a.title')).count()).not.toBeLessThan(25);
+  });
 });
