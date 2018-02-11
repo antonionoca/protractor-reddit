91c3fee4b17bafd7afc1f5b27fe9f9d09602f275
Antonio Noca <antonionoca@gmail.com>
2018-02-11 11:06:11 +0000

    Home Page Object (cont)

--- test/homepage.spec.js
+++ test/homepage.spec.js
@@ -9,22 +9,22 @@ describe('reddit homepage', function() {
   });
 
   it('should have 25 ranks', function() {
-    expect(element.all(by.css('.rank')).count()).not.toBeLessThan(homePage.RANKS);
+    expect(homePage.ranks.count()).not.toBeLessThan(homePage.RANKS);
   });
 
   it('should have 25 upvote arrows', function() {
-    expect(element.all(by.css('.arrow.up')).count()).not.toBeLessThan(homePage.UPVOTES);
+    expect(homePage.upvotes.count()).not.toBeLessThan(homePage.UPVOTES);
   });
 
   it('should have 25 downvote arrows', function() {
-    expect(element.all(by.css('.arrow.down')).count()).not.toBeLessThan(homePage.DOWNVOTES);
+    expect(homePage.downvotes.count()).not.toBeLessThan(homePage.DOWNVOTES);
   });
 
   it('should have 25 post headers', function() {
-    expect(element.all(by.css('a.title')).count()).not.toBeLessThan(homePage.HEADERS);
+    expect(homePage.headers.count()).not.toBeLessThan(homePage.HEADERS);
   });
 
   it('should show 18 links in the footer', function() {
-    expect(element.all(by.css('div.footer-parent a')).count()).not.toBeLessThan(homePage.FOOTER_LINKS);
+    expect(homePage.footerLinks.count()).not.toBeLessThan(homePage.FOOTER_LINKS);
   });
 });
