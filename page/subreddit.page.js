64144e62ce56649607dcc2fe093b6ae04d6d2cf1
Antonio Noca <antonionoca@gmail.com>
2018-02-14 10:06:23 +0000

    Updated subreddit page object

--- page/subreddit.page.js
+++ page/subreddit.page.js
@@ -3,6 +3,8 @@
 var SubredditsPage = function() {
   var self = this;
 
+  self.SUBREDDITS = 25;
+
   self.get = function() {
     browser.waitForAngularEnabled(false);
     browser.get('https://reddit.com/subreddits');
--- test/subreddits.spec.js
+++ test/subreddits.spec.js
@@ -9,14 +9,14 @@ describe('subreddits page', function() {
   });
 
   it('should list 25 subreddits to add', function() {
-    expect(element.all(by.css('a.add')).count()).not.toBeLessThan(25);
+    expect(element.all(by.css('a.add')).count()).not.toBeLessThan(subredditsPage.SUBREDDITS);
   });
 
   it('should have 25 subreddit titles', function() {
-    expect(element.all(by.css('a.title')).count()).not.toBeLessThan(25);
+    expect(element.all(by.css('a.title')).count()).not.toBeLessThan(subredditsPage.SUBREDDITS);
   });
 
   it('should have 25 subreddit short descriptions', function() {
-    expect(element.all(by.css('div.usertext-body')).count()).not.toBeLessThan(25);
+    expect(element.all(by.css('div.usertext-body')).count()).not.toBeLessThan(subredditsPage.SUBREDDITS);
   });
 });
